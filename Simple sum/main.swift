10debe089c96f595dc4a2252bd6cec9c8c67f2e1
BGDV <andrey.al.bogdanov@gmail.com>
2022-08-07 07:33:31 +0300

    first commit

--- Simple sum/main.swift
+++ Simple sum/main.swift
@@ -5,7 +5,23 @@
 //  Created by BGDV on 07.08.2022.
 //
 
+
+
+
+/*
+ 
+ A simple sum of two numbers.
+ If letters and not numbers are entered, they will be defined as zero.
+ 
+ */
+
 import Foundation
 
-print("Hello, World!")
+let valueOne = readLine()
+let valueTwo = readLine()
+
+let numberOne = Int(valueOne!) ?? 0
+let numberTwo = Int(valueTwo!) ?? 0
+
+print(numberOne + numberTwo)
 
